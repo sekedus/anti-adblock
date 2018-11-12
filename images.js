16830c4bd479753900c12665bd541013a5df1392
Dragon Warrior <sekedus@gmail.com>
2018-11-12 23:01:00 +0800

    Create images.js
    list image

---
+++ images.js
@@ -0,0 +1,9 @@
+data = {
+    "images": [{
+        "src": "http://googleads.g.doubleclick.net/favicon.ico"
+    }, {
+        "src": "http://admedia.com/images/favicon.ico"
+    }, {
+        "src": "https://mellowads.com/favicon.ico"
+    }]
+}
